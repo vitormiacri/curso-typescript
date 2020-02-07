75969ee4c9ec7f70337d8718bc16eedbda0ea2d8
Vitor Morelli <vitao.miacri@gmail.com>
2020-02-07 16:31:33 -0300

    Capitulo 4 - Namespaces

---
+++ namespaces/namespaces.js
@@ -0,0 +1,5 @@
+"use strict";
+///<reference path="geometriaCirc.ts" />
+///<reference path="geometriaRet.ts" />
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
