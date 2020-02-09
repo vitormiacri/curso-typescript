d2bb71caf603a4ae83f81be506410fc46486f267
Vitor Morelli <vitao.miacri@gmail.com>
2020-02-09 16:47:46 -0300

    Capitulo 5 - Interfaces

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,121 @@
+interface Humano {
+  nome: string; // propriedade obrigatoria
+  idade?: number; // propriedade opcional
+  [prop: string]: any; //propriedade dinamica
+  saudar(sobrenome: string): void; //função obrigatória que deve ser implementada pela instancia do objeto
+  // saudar?(sobrenome: string): void; //função opcional
+}
+
+function saudarComOla(pessoa: Humano) {
+  console.log("Olá, " + pessoa.nome);
+}
+
+function mudarNome(pessoa: Humano) {
+  pessoa.nome = "Joana";
+}
+
+const pessoa: Humano = {
+  nome: "João",
+  idade: 37,
+  saudar(sobrenome: string) {
+    console.log("Olá, meu nome é " + this.nome + " " + sobrenome);
+  }
+};
+
+saudarComOla(pessoa);
+mudarNome(pessoa);
+saudarComOla(pessoa);
+// saudarComOla({ nome: "Vitor", idade: 35, altura: 1.75 });
+pessoa.saudar("Morelli");
+
+// usando classes
+class Cliente implements Humano {
+  nome: string = "";
+  ultimaCompra: Date = new Date();
+
+  saudar(sobrenome: string) {
+    console.log("Olá, meu nome é " + this.nome + " " + sobrenome);
+  }
+}
+
+const meuCliente = new Cliente();
+meuCliente.nome = "Han";
+saudarComOla(meuCliente);
+meuCliente.saudar("Solo");
+console.log(meuCliente.ultimaCompra);
+
+// Interface de função
+interface FuncaoCalculo {
+  (a: number, b: number): number;
+}
+
+let potencia: FuncaoCalculo;
+
+potencia = function(base: number, exp: number): number {
+  // Lógica para calcular potencia "na não"
+  return Array(exp)
+    .fill(base)
+    .reduce((t, a) => t * a);
+};
+
+console.log(potencia(3, 10));
+console.log(Math.pow(3, 10));
+console.log(3 ** 10);
+
+// Herança
+interface A {
+  a(): void;
+}
+
+interface B {
+  b(): void;
+}
+
+interface ABC extends A, B {
+  c(): void;
+}
+
+class RealA implements A {
+  a(): void {}
+}
+
+class RealAB implements A, B {
+  a(): void {}
+  b(): void {}
+}
+
+class RealABC implements ABC {
+  a(): void {}
+  b(): void {}
+  c(): void {}
+}
+
+abstract class AbstrataABD implements A, B {
+  a(): void {}
+  b(): void {}
+  abstract d(): void;
+}
+
+interface Object {
+  log(): void;
+}
+
+Object.prototype.log = function() {
+  console.log(this.toString());
+};
+
+const x = 2;
+const y = 3;
+const z = 4;
+
+x.log();
+y.log();
+z.log();
+
+const cli = {
+  nome: "Pedro",
+  toString() {
+    return this.nome;
+  }
+};
+cli.log();
